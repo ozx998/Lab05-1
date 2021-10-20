a0eade7e94b899c68bdf07bddb056eb1b4ece72f
ozx998 <91934397+ozx998@users.noreply.github.com>
2021-10-20 22:06:30 +0800

    update
    did stuff

---
+++ Assets/CharacterScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class CharacterScript : MonoBehaviour
+{
+    public int score;
+        public GameObject scoreText;
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            score += 10;
+        }
+        if (collision.gameObject.tag == "water")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(score == 50)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+        scoreText.GetComponent<Text>().text = "Score: " + score;
+    }
+}
